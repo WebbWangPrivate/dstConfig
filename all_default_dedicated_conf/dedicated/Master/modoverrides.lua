6021aec6cfd2ef1e9f4e351ff558675faa59e55b
Webb Wang <wwb999@vip.qq.com>
2016-12-24 21:39:45 +0800

    latest dst server conf

---
+++ all_default_dedicated_conf/dedicated/Master/modoverrides.lua
@@ -0,0 +1,119 @@
+return {
+  ["workshop-358749986"]={
+    configuration_options={ IndicatorSize=3, MaxIndicator=7000, PlayerIndicators=1 },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-367546858"]={ configuration_options={ LANG="auto", SMALL_TEXTURES=true }, enabled=true },
+  ["workshop-368321978"]={ configuration_options={  }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=true,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-380079744"]={ configuration_options={ key2=122 }, enabled=true },
+  ["workshop-380122571"]={
+    configuration_options={
+      HPamount="yes",
+      Insomniac="yes",
+      MonsterMeat="yes",
+      ScytheDamage="57",
+      ScytheRecipeCost="normal",
+      SpeedBoost="1.3",
+      appearance="old",
+      lightAura="yes",
+      sanityAmount="120" 
+    },
+    enabled=true 
+  },
+  ["workshop-381565292"]={ configuration_options={  }, enabled=true },
+  ["workshop-399799824"]={ configuration_options={ key=122 }, enabled=true },
+  ["workshop-399803164"]={ configuration_options={  }, enabled=true },
+  ["workshop-412291722"]={ configuration_options={ Time="default" }, enabled=true },
+  ["workshop-431003525"]={ configuration_options={  }, enabled=true },
+  ["workshop-437711916"]={ configuration_options={ DROPGLASSES=1 }, enabled=true },
+  ["workshop-438211232"]={ configuration_options={  }, enabled=true },
+  ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
+  ["workshop-458940297"]={
+    configuration_options={
+      DFV_ClientPrediction="default",
+      DFV_FueledSettings="default",
+      DFV_Language="EN",
+      DFV_MinimalMode="default",
+      DFV_PercentReplace="default",
+      DFV_ShowACondition="default",
+      DFV_ShowADefence="default",
+      DFV_ShowAType="default",
+      DFV_ShowDamage="default",
+      DFV_ShowFireTime="default",
+      DFV_ShowInsulation="default",
+      DFV_ShowTemperature="default",
+      DFV_ShowUses="default" 
+    },
+    enabled=true 
+  },
+  ["workshop-461627386"]={ configuration_options={  }, enabled=true },
+  ["workshop-479243762"]={ configuration_options={  }, enabled=true },
+  ["workshop-482125281"]={ configuration_options={  }, enabled=true },
+  ["workshop-489184199"]={ configuration_options={  }, enabled=true },
+  ["workshop-498729626"]={ configuration_options={  }, enabled=true },
+  ["workshop-503919639"]={ configuration_options={ FIX_EQUIP_NOISE=false }, enabled=true },
+  ["workshop-540456786"]={ configuration_options={  }, enabled=true },
+  ["workshop-550193026"]={ configuration_options={  }, enabled=true },
+  ["workshop-551324730"]={ configuration_options={  }, enabled=true },
+  ["workshop-552215051"]={
+    configuration_options={
+      INCREASEBACKPACKSIZES_BACKPACK=8,
+      INCREASEBACKPACKSIZES_KRAMPUSSACK=14,
+      INCREASEBACKPACKSIZES_PIGGYBACK=12,
+      largericebox=9,
+      largertreasurechest=9 
+    },
+    enabled=true 
+  },
+  ["workshop-553665029"]={ configuration_options={  }, enabled=true },
+  ["workshop-572538624"]={
+    configuration_options={
+      IS_CHS_ALL_MOD=true,
+      IS_CHS_CHARACTER=true,
+      IS_CHS_FIX_ALL=true,
+      IS_CHS_SETTINGS=true 
+    },
+    enabled=true 
+  },
+  ["workshop-578326840"]={ configuration_options={  }, enabled=true },
+  ["workshop-591258809"]={ configuration_options={  }, enabled=true },
+  ["workshop-598857873"]={ configuration_options={  }, enabled=true },
+  ["workshop-600059870"]={ configuration_options={  }, enabled=true },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="yees",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  } 
+}
